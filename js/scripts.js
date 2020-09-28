bd070daa583e51c4919ca42f4765e726fca11d4c
Igor Barros <igor@refspatial.com.br>
2020-09-28 20:22:46 -0300

    Ta ai corno

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 const url_api = "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo";
 const users = [];
 
+
 function getUsersFromApi() {
     fetch(url_api, {
         method: 'get',
@@ -8,18 +9,16 @@ function getUsersFromApi() {
         headers: new Headers({
             'Content-Type': 'application/json'
           })
-    }).then(response => {
+    }).then(async (api_response) => {
         debugger;
-        response.text();
+        var response = await api_response.json();
+        
+        return response;
     }).catch(function (err) {
         console.error(err);
     });
 
-    /*let response = await fetch(url_api, {
-        method: 'get',
-        mode: 'cors'
-    });
-    let usersFat = JSON.parse(await response.text()).results;*/
+    //let usersFat = JSON.parse(await response.text()).results;*/
     //users = usersFat.map(({ gender, name.first + name.last, picture, dob.age }) => ({ gender, name, picture, age }))
 }
 
