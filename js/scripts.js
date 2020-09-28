6efb7ff5881e56880a212425b07b4ae38d3dd319
unknown <icaro.nove-nta@hotmail.com>
2020-09-28 20:29:15 -0300

    Parcial load users

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 const url_api = "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo";
-const users = [];
+var users = [];
 
-function getUsersFromApi() {
+
+function loadUsers() {
     fetch(url_api, {
         method: 'get',
         mode: 'cors',
         headers: new Headers({
             'Content-Type': 'application/json'
-          })
-    }).then(response => {
-        debugger;
-        response.text();
+        })
+    }).then(async (api_response) => {
+        var response = await api_response.json();
+        users = response.results.map(({ gender, name, picture, dob }) =>
+            ({ "gender": gender, "name": name.first + " " + name.last, "picture": picture.medium, "age": dob.age }));
     }).catch(function (err) {
         console.error(err);
     });
-
-    /*let response = await fetch(url_api, {
-        method: 'get',
-        mode: 'cors'
-    });
-    let usersFat = JSON.parse(await response.text()).results;*/
-    //users = usersFat.map(({ gender, name.first + name.last, picture, dob.age }) => ({ gender, name, picture, age }))
 }
 
-function loadUsers(event) {
+function showUsers(users){
+
+}
+
+function searchUsers(event) {
 
 }
